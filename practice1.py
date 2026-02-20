fbf149abf6cc92058becc813ce7586c675a5c41b
Apu59 <ssasarkar59@gmail.com>
2026-02-21 04:48:03 +0600

    First set of practice questions

---
+++ practice1.py
@@ -0,0 +1,29 @@
+
+#WAP a program to input 2 numbers and print their sum
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+sum = (a + b)
+print("The sum is : ",sum)
+
+
+#WAP to input side of a square & print its area
+
+side = int(input("Enter the side of a square :"))
+area = (side * side)
+print("The area is :",area)
+
+#WAP to input 2 floating poitn numbers and print their average
+
+num1 = float(input("Enter first floating number :"))
+num2 = float(input("Enter second floating number : "))
+avg = (num1 + num2)/2
+print("The average is : ",avg)
+
+#WAP to input 2 int number x and y and print True if x is greter than or equal to y , if not the print false 
+
+x = int(input("Enter the first int value :"))
+y = int(input("Enter the second int value :"))
+
+print(x >= y)
+
