548287d08af81d11df87ef2d98a15a0eb24ba5c8
Apu59 <ssasarkar59@gmail.com>
2026-02-22 05:33:49 +0600

    Second set of practice questions

---
+++ practice2.py
@@ -0,0 +1,12 @@
+#WAP to input user's first name and print its length
+
+fName = input("Write your first name here : ")
+print("The length of the first name is :",len(fName))
+
+
+#WAP to find the occurence of $ in a string 
+
+str = "I have lots of $, do you need some $?"
+print("There are ",str.count("$"), "occurence of $ in the string.")
+
+
