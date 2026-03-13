eaf9ddfd57e5daefed1b6f81e2bbea1d9fd5ec21
Apu59 <ssasarkar59@gmail.com>
2026-03-13 23:27:45 +0600

    Lecture 7 practice question

---
+++ practice7(1).py
@@ -0,0 +1,18 @@
+#WAF to find in which line of the file does the word "learning" occur first. Print -1 if the word is not found.
+
+def check_for_line() :
+    word = "programming"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f :
+        while data :
+            data = f.readline()
+            if(word in data) :
+                print(line_no)
+                return
+            line_no += 1
+
+    return -1
+
+
+check_for_line()
