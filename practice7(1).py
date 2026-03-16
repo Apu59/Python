79ae9f7151eb268fb2ba3398743e19812d19a4db
Apu59 <ssasarkar59@gmail.com>
2026-03-16 17:03:11 +0600

    Lecture 7 pratice questions complete

--- practice7(1).py
+++ practice7(1).py
@@ -16,3 +16,30 @@ def check_for_line() :
 
 
 check_for_line()
+
+
+
+#From a file containing numbers separated by comma, print the count of even numbers.
+
+with open("practice2.txt", "r") as f :
+    data2 = f.read()
+count = 0
+nums = data2.split(",")
+for val in nums :
+    if(int(val) %2 == 0) :
+        count += 1
+
+print(count)  
+
+
+#This is the raw program of parsing comma separated values manually 
+#     num = ""
+#     for i in range(len(data2)) :
+#         if(data2[i] == ",") :
+#             print(int(num))
+#             num = ""
+#         else:
+#             num += data2[i]
+# print(int(num)) #Above program will not print the last value that's why we use this
+
+
